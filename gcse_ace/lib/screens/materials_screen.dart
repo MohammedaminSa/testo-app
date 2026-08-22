c20141342c773a31de71dde54aafea92b8c7d1d2
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-08-22 15:42:47 +0300

    Add gcse_ace/lib/screens/materials_screen.dart

---
+++ gcse_ace/lib/screens/materials_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MaterialsScreen extends StatelessWidget {
+  const MaterialsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Materials')),
+      body: const Center(child: Text('Study materials coming soon')),
+    );
+  }
+}
