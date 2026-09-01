fb05bf966ef786f40b1d1536431b4994275fca7d
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-09-01 08:46:23 +0300

    Add Riverpod data providers

---
+++ gcse_ace/lib/providers/data_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/department.dart';
+import '../models/paper.dart';
+import '../models/question.dart';
+import '../models/option.dart';
+import '../services/data_service.dart';
+
+final departmentsProvider = FutureProvider<List<Department>>((ref) async {
+  return DataService.instance.getDepartments();
+});
+
+final papersProvider = FutureProvider<List<Paper>>((ref) async {
+  return DataService.instance.getPapers();
+});
+
+final questionsProvider =
+    FutureProvider.family<List<Question>, String>((ref, paperId) async {
+  return DataService.instance.getQuestions(paperId);
+});
+
+final optionsProvider =
+    FutureProvider.family<List<Option>, String>((ref, questionId) async {
+  return DataService.instance.getOptions(questionId);
+});
