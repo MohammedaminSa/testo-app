c9c84c193d9ed5cbfbae1ca43e4ce58e23c43c9a
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-09-01 08:46:17 +0300

    Add DataService for Supabase queries

---
+++ gcse_ace/lib/services/data_service.dart
@@ -0,0 +1,63 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../models/department.dart';
+import '../models/paper.dart';
+import '../models/question.dart';
+import '../models/option.dart';
+
+class DataService {
+  DataService._();
+
+  static final DataService instance = DataService._();
+
+  SupabaseClient get _client => Supabase.instance.client;
+
+  Future<List<Department>> getDepartments() async {
+    final data = await _client.from('departments').select();
+    return (data as List).map((json) => Department.fromJson(json)).toList();
+  }
+
+  Future<List<Paper>> getPapers({String? departmentId}) async {
+    var query = _client.from('papers').select();
+    if (departmentId != null) {
+      query = query.eq('department_id', departmentId);
+    }
+    final data = await query.order('year', ascending: false);
+    return (data as List).map((json) => Paper.fromJson(json)).toList();
+  }
+
+  Future<List<Question>> getQuestions(String paperId) async {
+    final data = await _client
+        .from('questions')
+        .select()
+        .eq('paper_id', paperId)
+        .order('order_number');
+    return (data as List).map((json) => Question.fromJson(json)).toList();
+  }
+
+  Future<List<Option>> getOptions(String questionId) async {
+    final data = await _client
+        .from('options')
+        .select()
+        .eq('question_id', questionId)
+        .order('letter');
+    return (data as List).map((json) => Option.fromJson(json)).toList();
+  }
+
+  Future<Map<String, List<Option>>> getOptionsForQuestions(
+    List<String> questionIds,
+  ) async {
+    final data = await _client
+        .from('options')
+        .select()
+        .inFilter('question_id', questionIds)
+        .order('letter');
+
+    final map = <String, List<Option>>{};
+    for (final json in data as List) {
+      final option = Option.fromJson(json);
+      map.putIfAbsent(option.questionId, () => []).add(option);
+    }
+    return map;
+  }
+}
