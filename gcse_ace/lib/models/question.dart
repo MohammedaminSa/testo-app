39a275142eaa0d12072b44f56a54781b2da860bd
Mohammed Amin <mhmmdmnsdk@gmail.com>
2026-09-03 17:41:22 +0300

    Add options field to Question model

--- gcse_ace/lib/models/question.dart
+++ gcse_ace/lib/models/question.dart
@@ -1,3 +1,5 @@
+import 'option.dart';
+
 class Question {
   final String id;
   final String paperId;
@@ -7,6 +9,7 @@ class Question {
   final String questionType;
   final String? correctAnswer;
   final String? explanation;
+  final List<Option>? options;
 
   const Question({
     required this.id,
@@ -17,6 +20,7 @@ class Question {
     required this.questionType,
     this.correctAnswer,
     this.explanation,
+    this.options,
   });
 
   factory Question.fromJson(Map<String, dynamic> json) {
@@ -29,6 +33,9 @@ class Question {
       questionType: json['question_type'] as String,
       correctAnswer: json['correct_answer'] as String?,
       explanation: json['explanation'] as String?,
+      options: json['options'] != null
+          ? (json['options'] as List).map((o) => Option.fromJson(o)).toList()
+          : null,
     );
   }
 }
